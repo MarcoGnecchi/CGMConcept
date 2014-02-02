4eb84614af09644236b1eb197b1450cca7d5b40c
Marco <marco.gnecchi@gmail.com>
2014-02-02 19:16:15 +0000

    Implement validation

--- src/com/cgmconcept/SteelDrawingActivity.java
+++ src/com/cgmconcept/SteelDrawingActivity.java
@@ -3,15 +3,51 @@ package com.cgmconcept;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class SteelDrawingActivity extends Activity {
+import com.mobsandgeeks.saripaar.Rule;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.Validator.ValidationListener;
+import com.mobsandgeeks.saripaar.annotation.NumberRule;
+import com.mobsandgeeks.saripaar.annotation.NumberRule.NumberType;
+import com.mobsandgeeks.saripaar.annotation.Required;
+
+public class SteelDrawingActivity extends Activity implements
+		ValidationListener, OnClickListener {
+
+	@Required(order = 1)
+	private EditText inletEditText;
+
+	@Required(order = 2)
+	private EditText outletEditText;
+
+	@Required(order = 3)
+	@NumberRule(order = 4, type = NumberType.INTEGER, gt = 1, lt = 10, message="Please enter a number between 1 and 10")
+	private EditText nOfBlockEditText;
+
+	@Required(order = 5)
+	private EditText targetSpeedEditText;
+
+	private Button confirmButton; 
+	
+	private Validator validator;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_steel_drawing);
+
+		inletEditText = (EditText) findViewById(R.id.txtinlet);
+		outletEditText = (EditText) findViewById(R.id.txtoutlet);
+		nOfBlockEditText = (EditText) findViewById(R.id.txtnofblocks);
+		targetSpeedEditText = (EditText) findViewById(R.id.txttargetspeed);
+		confirmButton = (Button) findViewById(R.id.btnSubmit);
 
 		// Setting the spinner
 		Spinner spinner = (Spinner) findViewById(R.id.typeofwire);
@@ -20,6 +56,12 @@ public class SteelDrawingActivity extends Activity {
 						android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinner.setAdapter(adapter);
+
+		validator = new Validator(this);
+		validator.setValidationListener(this);
+		
+		confirmButton.setOnClickListener(this);
+
 	}
 
 	@Override
@@ -27,6 +69,31 @@ public class SteelDrawingActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.steel_drawing, menu);
 		return true;
+	}
+
+	@Override
+	public void onValidationSucceeded() {
+
+		Toast.makeText(this, "Yay! we got it right!", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onValidationFailed(View failedView, Rule<?> failedRule) {
+
+		String message = failedRule.getFailureMessage();
+
+		if (failedView instanceof EditText) {
+			failedView.requestFocus();
+			((EditText) failedView).setError(message);
+		} else {
+			Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		
+		validator.validate();
 	}
 
 }
