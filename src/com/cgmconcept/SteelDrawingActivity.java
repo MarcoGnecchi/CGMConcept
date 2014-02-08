c54c3cf55a9366dcbb32f5e15ea3b7e862ded788
Marco <marco.gnecchi@gmail.com>
2014-02-08 23:31:48 +0000

    Introduced numpickers

--- src/com/cgmconcept/SteelDrawingActivity.java
+++ src/com/cgmconcept/SteelDrawingActivity.java
@@ -1,14 +1,14 @@
 package com.cgmconcept;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.NumberPicker;
 import android.widget.Toast;
 
 import com.cgmconcept.model.StreelDrawing;
@@ -29,13 +29,14 @@ public class SteelDrawingActivity extends Activity implements
 	private EditText outletEditText;
 
 	@Required(order = 3)
-	@NumberRule(order = 4, type = NumberType.INTEGER, gt = 1, lt = 10, message="Please enter a number between 1 and 10")
-	private EditText nOfBlockEditText;
+	private NumberPicker nOfDiesPicker;
+
+	@Required(order = 3)
+	private NumberPicker carbonContentPicker;
 
 	@Required(order = 5)
 	private EditText targetSpeedEditText;
 	
-	private Spinner spinner;
 
 	private Button confirmButton; 
 	
@@ -45,26 +46,28 @@ public class SteelDrawingActivity extends Activity implements
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_steel_drawing);
-
 		inletEditText = (EditText) findViewById(R.id.txtinlet);
 		outletEditText = (EditText) findViewById(R.id.txtoutlet);
-		nOfBlockEditText = (EditText) findViewById(R.id.txtnofblocks);
+		
+		nOfDiesPicker = (NumberPicker) findViewById(R.id.n_of_dies);
+		nOfDiesPicker.setMaxValue(10);
+		nOfDiesPicker.setMinValue(1);
+		
+		
+		carbonContentPicker = (NumberPicker) findViewById(R.id.carbon_content);
+		String[] carbonValues = getResources().getStringArray(R.array.carbon_content_array);
+		carbonContentPicker.setDisplayedValues(carbonValues);
+		carbonContentPicker.setMinValue(0);
+		carbonContentPicker.setMaxValue(carbonValues.length - 1);
+		
+		
 		targetSpeedEditText = (EditText) findViewById(R.id.txttargetspeed);
 		confirmButton = (Button) findViewById(R.id.btnSubmit);
-
-		// Setting the spinner
-		spinner = (Spinner) findViewById(R.id.typeofwire);
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter
-				.createFromResource(this, R.array.wires_array,
-						android.R.layout.simple_spinner_item);
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner.setAdapter(adapter);
 
 		validator = new Validator(this);
 		validator.setValidationListener(this);
 		
 		confirmButton.setOnClickListener(this);
-
 	}
 
 	@Override
@@ -81,9 +84,10 @@ public class SteelDrawingActivity extends Activity implements
 		sd.setInlet(Integer.valueOf(inletEditText.getText().toString()));
 		sd.setOutlet(Integer.valueOf(inletEditText.getText().toString()));
 		sd.setNOfBlocks(Integer.valueOf(inletEditText.getText().toString()));
-		sd.setTypeOfWire(spinner.getSelectedItemPosition());
 		sd.setTargetSpeed(Integer.valueOf(inletEditText.getText().toString()));
-		
+		Intent i = new Intent(this, ConfirmData.class);
+		startActivity(i);
+				
 	}
 
 	@Override
