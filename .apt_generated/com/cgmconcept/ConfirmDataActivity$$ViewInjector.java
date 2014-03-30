ee747ca58a5c83a3fc26d74b0967ac22489c7f75
Marco <marco.gnecchi@gmail.com>
2014-03-30 23:35:00 +0100

    improve UI for show result table

--- .apt_generated/com/cgmconcept/ConfirmDataActivity$$ViewInjector.java
+++ .apt_generated/com/cgmconcept/ConfirmDataActivity$$ViewInjector.java
@@ -7,6 +7,36 @@ import butterknife.ButterKnife.Finder;
 public class ConfirmDataActivity$$ViewInjector {
   public static void inject(Finder finder, final com.cgmconcept.ConfirmDataActivity target, Object source) {
     View view;
+    view = finder.findById(source, 2130968598);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2130968598' for field 'targetSpeed' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.targetSpeed = (android.widget.TextView) view;
+    view = finder.findById(source, 2130968602);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2130968602' for field 'inletTS' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.inletTS = (android.widget.TextView) view;
+    view = finder.findById(source, 2130968597);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2130968597' for field 'carbonContent' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.carbonContent = (android.widget.TextView) view;
+    view = finder.findById(source, 2130968595);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2130968595' for field 'outlet' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.outlet = (android.widget.TextView) view;
+    view = finder.findById(source, 2130968596);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2130968596' for field 'nOfDies' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.nOfDies = (android.widget.TextView) view;
+    view = finder.findById(source, 2130968600);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2130968600' for field 'averagereduction' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.averagereduction = (android.widget.TextView) view;
     view = finder.findById(source, 2130968604);
     if (view == null) {
       throw new IllegalStateException("Required view with id '2130968604' for field 'btnCalculate' and method 'submit' was not found. If this view is optional add '@Optional' annotation.");
@@ -20,69 +50,39 @@ public class ConfirmDataActivity$$ViewInjector {
           target.submit();
         }
       });
-    view = finder.findById(source, 2130968596);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968596' for field 'nOfDies' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.nOfDies = (android.widget.TextView) view;
-    view = finder.findById(source, 2130968595);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968595' for field 'outlet' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.outlet = (android.widget.TextView) view;
-    view = finder.findById(source, 2130968597);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968597' for field 'carbonContent' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.carbonContent = (android.widget.TextView) view;
-    view = finder.findById(source, 2130968600);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968600' for field 'averagereduction' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.averagereduction = (android.widget.TextView) view;
-    view = finder.findById(source, 2130968603);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968603' for field 'outletTS' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.outletTS = (android.widget.TextView) view;
-    view = finder.findById(source, 2130968601);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968601' for field 'totalReduction' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.totalReduction = (android.widget.TextView) view;
-    view = finder.findById(source, 2130968598);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968598' for field 'targetSpeed' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.targetSpeed = (android.widget.TextView) view;
-    view = finder.findById(source, 2130968602);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968602' for field 'inletTS' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.inletTS = (android.widget.TextView) view;
-    view = finder.findById(source, 2130968594);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968594' for field 'inlet' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.inlet = (android.widget.TextView) view;
     view = finder.findById(source, 2130968599);
     if (view == null) {
       throw new IllegalStateException("Required view with id '2130968599' for field 'speedwireinlet' was not found. If this view is optional add '@Optional' annotation.");
     }
     target.speedwireinlet = (android.widget.TextView) view;
+    view = finder.findById(source, 2130968603);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2130968603' for field 'outletTS' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.outletTS = (android.widget.TextView) view;
+    view = finder.findById(source, 2130968594);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2130968594' for field 'inlet' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.inlet = (android.widget.TextView) view;
+    view = finder.findById(source, 2130968601);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2130968601' for field 'totalReduction' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.totalReduction = (android.widget.TextView) view;
   }
 
   public static void reset(com.cgmconcept.ConfirmDataActivity target) {
-    target.btnCalculate = null;
-    target.nOfDies = null;
-    target.outlet = null;
-    target.carbonContent = null;
-    target.averagereduction = null;
-    target.outletTS = null;
-    target.totalReduction = null;
     target.targetSpeed = null;
     target.inletTS = null;
-    target.inlet = null;
+    target.carbonContent = null;
+    target.outlet = null;
+    target.nOfDies = null;
+    target.averagereduction = null;
+    target.btnCalculate = null;
     target.speedwireinlet = null;
+    target.outletTS = null;
+    target.inlet = null;
+    target.totalReduction = null;
   }
 }
--- src/com/cgmconcept/ShowResultsActivity.java
+++ src/com/cgmconcept/ShowResultsActivity.java
@@ -33,7 +33,7 @@ public class ShowResultsActivity extends Activity {
 	    GridLayout gd = (GridLayout) findViewById(R.id.gd_show_result);
 	    
 	    for (int i = 1; i <= mSteelDrawing.getNOfDies(); i++) {
-			
+			int layout = ((i % 2) == 0) ? R.layout.result_item_pair : R.layout.result_item_odd;
 	    	gd.setRowCount(gd.getRowCount() + 1);
 	    	
 	    	TextView tv = new TextView(this);
@@ -43,42 +43,42 @@ public class ShowResultsActivity extends Activity {
 	    	gd.addView(tv);
 	    	
 	    	
-	    	View diameterView = getLayoutInflater().inflate(R.layout.result_item, null);
+	    	View diameterView = getLayoutInflater().inflate(layout, null);
 	    	TextView diameterValue = (TextView) diameterView.findViewById(R.id.text);
 	    	diameterValue.setText(String.format( "%.2f", mSteelDrawing.getDiameter(i)));
 	    	gd.addView(diameterView);
 	    	
-	    	View reductionView = getLayoutInflater().inflate(R.layout.result_item, null);
+	    	View reductionView = getLayoutInflater().inflate(layout, null);
 	    	TextView reductionValue = (TextView) reductionView.findViewById(R.id.text);
 	    	reductionValue.setText(String.format( "%.2f%%", mSteelDrawing.getReduction(i)*100));
 	    	gd.addView(reductionValue);
 	    	
-	    	View totReductionView = getLayoutInflater().inflate(R.layout.result_item, null);
+	    	View totReductionView = getLayoutInflater().inflate(layout, null);
 	    	TextView totReductionValue = (TextView) totReductionView.findViewById(R.id.text);
 	    	totReductionValue.setText(String.format( "%.2f%%", mSteelDrawing.getTotalReductionAtStep(i)));
 	    	gd.addView(totReductionValue);
 	    	
-	    	View speedView = getLayoutInflater().inflate(R.layout.result_item, null);
+	    	View speedView = getLayoutInflater().inflate(layout, null);
 	    	TextView speedValue = (TextView) speedView.findViewById(R.id.text);
 	    	speedValue.setText(String.format( "%.2f", mSteelDrawing.getSpeed(i)));
 	    	gd.addView(speedValue);
 	    	
-	    	View outletTSViewKG = getLayoutInflater().inflate(R.layout.result_item, null);
+	    	View outletTSViewKG = getLayoutInflater().inflate(layout, null);
 	    	TextView outletTSValueKG = (TextView) outletTSViewKG.findViewById(R.id.text);
 	    	outletTSValueKG.setText(String.format( "%.2f", mSteelDrawing.getOutletTSKG(i)));
 	    	gd.addView(outletTSViewKG);
 	    	
-	    	View outletTSView = getLayoutInflater().inflate(R.layout.result_item, null);
+	    	View outletTSView = getLayoutInflater().inflate(layout, null);
 	    	TextView outletTSValue = (TextView) outletTSView.findViewById(R.id.text);
 	    	outletTSValue.setText(String.format( "%.2f", mSteelDrawing.getOutletTS(i)));
 	    	gd.addView(outletTSView);
 	    	
-	    	View pullView = getLayoutInflater().inflate(R.layout.result_item, null);
+	    	View pullView = getLayoutInflater().inflate(layout, null);
 	    	TextView pullValue = (TextView) pullView.findViewById(R.id.text);
 	    	pullValue.setText(String.format( "%.2f", mSteelDrawing.getPull(i)));
 	    	gd.addView(pullView);
 	    	
-	    	View powerView = getLayoutInflater().inflate(R.layout.result_item, null);
+	    	View powerView = getLayoutInflater().inflate(layout, null);
 	    	TextView powerValue = (TextView) powerView.findViewById(R.id.text);
 	    	powerValue.setText(String.format( "%.2f", mSteelDrawing.getPower(i)));
 	    	gd.addView(powerView);
