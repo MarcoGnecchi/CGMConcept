e2f0fe033b83b0c024976af650feb1cdd156b038
Marco Gnecchi <marco.gnecchi@genie-connect.com>
2014-04-01 17:30:52 +0100

    add documentation for new table view

--- .apt_generated/com/cgmconcept/ConfirmDataActivity$$ViewInjector.java
+++ .apt_generated/com/cgmconcept/ConfirmDataActivity$$ViewInjector.java
@@ -9,32 +9,42 @@ public class ConfirmDataActivity$$ViewInjector {
     View view;
     view = finder.findById(source, 2130968603);
     if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968603' for field 'speedwireinlet' was not found. If this view is optional add '@Optional' annotation.");
+      throw new IllegalStateException("Required view with id '2130968603' for field 'outletTS' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.outletTS = (android.widget.TextView) view;
+    view = finder.findById(source, 2130968599);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2130968599' for field 'speedwireinlet' was not found. If this view is optional add '@Optional' annotation.");
     }
     target.speedwireinlet = (android.widget.TextView) view;
-    view = finder.findById(source, 2130968602);
+    view = finder.findById(source, 2130968594);
     if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968602' for field 'targetSpeed' was not found. If this view is optional add '@Optional' annotation.");
+      throw new IllegalStateException("Required view with id '2130968594' for field 'inlet' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.inlet = (android.widget.TextView) view;
+    view = finder.findById(source, 2130968595);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2130968595' for field 'outlet' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.outlet = (android.widget.TextView) view;
+    view = finder.findById(source, 2130968597);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2130968597' for field 'carbonContent' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.carbonContent = (android.widget.TextView) view;
+    view = finder.findById(source, 2130968598);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2130968598' for field 'targetSpeed' was not found. If this view is optional add '@Optional' annotation.");
     }
     target.targetSpeed = (android.widget.TextView) view;
-    view = finder.findById(source, 2130968606);
+    view = finder.findById(source, 2130968602);
     if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968606' for field 'inletTS' was not found. If this view is optional add '@Optional' annotation.");
+      throw new IllegalStateException("Required view with id '2130968602' for field 'inletTS' was not found. If this view is optional add '@Optional' annotation.");
     }
     target.inletTS = (android.widget.TextView) view;
     view = finder.findById(source, 2130968604);
     if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968604' for field 'averagereduction' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.averagereduction = (android.widget.TextView) view;
-    view = finder.findById(source, 2130968596);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968596' for field 'nOfDies' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.nOfDies = (android.widget.TextView) view;
-    view = finder.findById(source, 2130968608);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968608' for field 'btnCalculate' and method 'submit' was not found. If this view is optional add '@Optional' annotation.");
+      throw new IllegalStateException("Required view with id '2130968604' for field 'btnCalculate' and method 'submit' was not found. If this view is optional add '@Optional' annotation.");
     }
     target.btnCalculate = (android.widget.Button) view;
     view.setOnClickListener(
@@ -45,44 +55,34 @@ public class ConfirmDataActivity$$ViewInjector {
           target.submit();
         }
       });
-    view = finder.findById(source, 2130968601);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968601' for field 'outlet' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.outlet = (android.widget.TextView) view;
-    view = finder.findById(source, 2130968607);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968607' for field 'outletTS' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.outletTS = (android.widget.TextView) view;
-    view = finder.findById(source, 2130968597);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968597' for field 'carbonContent' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.carbonContent = (android.widget.TextView) view;
-    view = finder.findById(source, 2130968605);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968605' for field 'totalReduction' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.totalReduction = (android.widget.TextView) view;
     view = finder.findById(source, 2130968600);
     if (view == null) {
-      throw new IllegalStateException("Required view with id '2130968600' for field 'inlet' was not found. If this view is optional add '@Optional' annotation.");
+      throw new IllegalStateException("Required view with id '2130968600' for field 'averagereduction' was not found. If this view is optional add '@Optional' annotation.");
     }
-    target.inlet = (android.widget.TextView) view;
+    target.averagereduction = (android.widget.TextView) view;
+    view = finder.findById(source, 2130968596);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2130968596' for field 'nOfDies' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.nOfDies = (android.widget.TextView) view;
+    view = finder.findById(source, 2130968601);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2130968601' for field 'totalReduction' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.totalReduction = (android.widget.TextView) view;
   }
 
   public static void reset(com.cgmconcept.ConfirmDataActivity target) {
+    target.outletTS = null;
     target.speedwireinlet = null;
+    target.inlet = null;
+    target.outlet = null;
+    target.carbonContent = null;
     target.targetSpeed = null;
     target.inletTS = null;
+    target.btnCalculate = null;
     target.averagereduction = null;
     target.nOfDies = null;
-    target.btnCalculate = null;
-    target.outlet = null;
-    target.outletTS = null;
-    target.carbonContent = null;
     target.totalReduction = null;
-    target.inlet = null;
   }
 }
